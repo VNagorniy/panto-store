fe6bdfba3c3683dec1e3099b263fad68cde785ff
VNagorniy <vnagorniyrnd@gmail.com>
2023-12-18 22:03:10 +0300

    подключение swiperjs на карточки в секции products

--- js/main.js
+++ js/main.js
@@ -22,3 +22,35 @@ document.addEventListener('click', function () {
 for (let hint of infoHints) {
   hint.addEventListener('click', (e) => e.stopPropagation());
 }
+
+// Swiper slider
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  freeMode: true,
+
+  slidesPerView: 4,
+  paceBetween: 42,
+
+  // breakpoints: {
+  //   640: {
+  //     slidesPerView: 2,
+  //     spaceBetween: 20,
+  //   },
+  //   768: {
+  //     slidesPerView: 4,
+  //     spaceBetween: 40,
+  //   },
+  //   1024: {
+  //     slidesPerView: 5,
+  //     spaceBetween: 50,
+  //   },
+  // },
+
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '#sliderNext',
+    prevEl: '#sliderPrev',
+  },
+});
