ebc218a389132b139da946cea87ef12adcae8591
VNagorniy <vnagorniyrnd@gmail.com>
2023-12-19 22:30:59 +0300

    подготовка табов(их функциональность) в секции products

--- js/main.js
+++ js/main.js
@@ -54,3 +54,33 @@ const swiper = new Swiper('.swiper', {
     prevEl: '#sliderPrev',
   },
 });
+
+
+//Tabs
+
+const tabsBtns = document.querySelectorAll('[data-tab]')
+const tabsProducts = document.querySelectorAll('[data-tab-value]')
+
+for (let btn of tabsBtns) {
+  btn.addEventListener('click', function() {
+    for (let btn of tabsBtns) {
+      btn.classList.remove('tab-controls__btn--active')
+    }
+
+    this.classList.add('tab-controls__btn--active')
+
+    for (let product of tabsProducts) {
+      if(this.dataset.tab === 'all') {
+        product.classList.remove('none');
+      } else {
+        if(product.dataset.tabValue === this.dataset.tab) {
+          product.classList.remove('none');
+        } else {
+          product.classList.add('none');
+        }
+      }   
+    }
+
+  swiper.update()
+  })
+} 
