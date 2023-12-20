c71d49b142c6bc1fdbe8a0d139ef451399fb7e53
VNagorniy <vnagorniyrnd@gmail.com>
2023-12-20 19:47:02 +0300

    фикc - закрытие подсказки, если выбрана другая

--- js/main.js
+++ js/main.js
@@ -5,6 +5,13 @@ const infoHints = document.querySelectorAll('.info-hint');
 for (let btn of infoBtns) {
   btn.addEventListener('click', function (e) {
     e.stopPropagation();
+
+    // Hide all hint
+    for (let hint of infoHints) {
+      hint.classList.add('none');
+    }
+
+    // Show current hint
     this.parentNode.querySelector('.info-hint').classList.toggle('none');
   });
 }
